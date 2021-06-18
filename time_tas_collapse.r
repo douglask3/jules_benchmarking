58f10867da33465bb023754b8c0d82d0d0b7efd0
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2021-06-18 14:29:24 +0100

    tidying and moving faster finder to own funtion

--- time_tas_collapse.r
+++ time_tas_collapse.r
@@ -1,28 +1,22 @@
 source("cfg.r")
 
-jules_dir = "../jules_outputs/"
-hist_mod = "u-bi607_Hist_NF_new"
+jules_dir = "/hpc/data/d05/cburton/jules_output/"
+hist_mod = "u-bi607_HIST_NF"
+outstream = "S2.Annual"
 
 Hist_yrs = 1990:2014
 
-varname = "cv"
+varname = "cveg"
 extent = c(-180, 180, -90, 90)
 
-dat = openMod(hist_mod, jules_dir, varname, Hist_yrs, 1)
+dat = openMod(hist_mod, jules_dir, varname, Hist_yrs, 1, stream = outstream)
 
-nn= 0
 findPntsInterest <- function(y, x, bins, xbin) {
-    nn<<-nn+1
-    print(nn)
     out = rep(NaN, 8)
     if (sum(y) < 1) return(out)
     if (length(unique(y))<5) return(out)
-    #ys = filter(y, rep(1/sbin, sbin), sides = 2)
     
-    binned = mapply(function(l, u) y[x>=l & x<=u], bins[,1], bins[,2],  SIMPLIFY = FALSE)#head(bins, -1), bins[-1], SIMPLIFY = FALSE)
-    #pv = mapply(function(b1, b2) wilcox.test(b1, b2, paired = F)[[3]],
-    #                        head(binned, -1), binned[-1])
-    
+    binned = mapply(function(l, u) y[x>=l & x<=u], bins[,1], bins[,2],  SIMPLIFY = FALSE)
     nl = length(binned)
     mbin = sapply(binned, mean)
 
@@ -60,24 +54,29 @@ findPntsInterest <- function(y, x, bins, xbin) {
     return(out)
 }
 
-mask = !is.na(dat[[1]])
-vdat = dat[mask]
+findMax_andMaxLoss <- function(dat, ts) {
+    mask = !is.na(dat[[1]])
+    vdat = dat[mask]
 
-if (nlayers(dat) == length(Hist_yrs)) {
-    xs = Hist_yrs
-} else {
-    if (nlayers(dat) == 12*length(Hist_yrs))
-        xs = head(seq(Hist_yrs[1], tail(Hist_yrs, 1)+1, length.out = nlayers(dat)+1),-1)
-    else browser()
+    if (nlayers(dat) == length(ts)) {
+        xs = ts
+    } else {
+        if (nlayers(dat) == 12*length(ts))
+            xs = head(seq(ts[1], tail(ts, 1)+1, length.out = nlayers(dat)+1),-1)
+        else browser()
+    }
+
+    bins = cbind(xs[1:(length(xs)-1)], xs[2:length(xs)])
+    xbin = apply(bins, 1, mean)
+    # = seq(min(xs), max(xs))
+    pnts = apply(vdat, 1, findPntsInterest, xs, bins, xbin)
+
+    out = dat[[1:8]]
+    out[mask] = t(pnts)
+    names(out) = c("MaxPnt", "MaxPval", "MaxRangeMin", "MaxRangeMax",
+                   "LossPnt", "LossPval", "LossRangeMin", "LossRangeMax")
+    return(out)
 }
 
 
-bins = cbind(xs[1:(length(xs)-23)], xs[24:length(xs)])
-xbin = apply(bins, 1, mean)
-# = seq(min(xs), max(xs))
-#pnts = apply(vdat, 1, findPntsInterest, xs, bins, xbin)
-
-out = dat[[1:8]]
-out[mask] = t(pnts)
-
-
+dat = findMax_andMaxLoss(dat, Hist_yrs)
