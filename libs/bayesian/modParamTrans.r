6eff3a91a1b716d303c93df4b79e1a836c674b17
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2021-08-26 23:09:54 +0100

    adding a if y is 1 stabalised on [0,inf] bounded parameters, and adding a [0,1] paramter option

--- libs/bayesian/modParamTrans.r
+++ libs/bayesian/modParamTrans.r
@@ -1,6 +1,11 @@
 
 modParamTrans.zeroInf <- function(x, a = -1) 1-exp(a*x)
 
-modParamTrans.zeroInf.inverse <- function(y, a = -1) (1/a)* log(1-y)
+modParamTrans.zeroInf.inverse <- function(y, a = -1) {
+    y[y ==1] = 1 - 1E-10
+    (1/a)* log(1-y)
+}
 
+modParamTrans.zeroOne <- function(x, ..) x
 
+modParamTrans.zeroOne.inverse <- function(x, ..) x
