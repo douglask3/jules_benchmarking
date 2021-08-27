98dfbea870c6b5b8dbdbe16e7f4ce614860c449f
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2021-08-27 09:55:08 +0100

    finishing off plotting and adding new params info, which is also outputted

--- libs/bayesian/optiPlot2D.r
+++ libs/bayesian/optiPlot2D.r
@@ -101,7 +101,6 @@ optiPlot2D <- function() {
     ps0 = ps
     matrix2list <- function(x) 
         lapply(apply(x, 2, list), function(i) i[[1]])
-        
     
     pntPriors = mapply(CalPrior, param_trans, matrix2list(t(paramsP)))
     
@@ -110,10 +109,9 @@ optiPlot2D <- function() {
     ps[is.infinite(ps)] = min(y[!is.infinite(y)])
     
     ## setting up plot
-    #xrange = range(c(params, x)); yrange = range(c(ps, y[!is.infinite(y)]))
-    dev.new()
-    layout(rbind(c(1, 3, 5), c(2, 4, 6), 7), height = c(0.7, 0.3, 1))
-    par(mar = c(2.5, .5, 0.5, 0.5), oma = c(0, 3, 1.5, 0))
+    
+    layout(rbind(c(1, 3, 5), c(2, 4, 6), 7), height = c(0.65, 0.15, 1))
+    par(mar = c(3, .5, 0.5, 0.5), oma = c(0.1, 3, 1.5, 0))
     
     imageP <- function(Pz, psZ = NULL, cols, title = '', 
                        xaxt = TRUE, xlbt = FALSE, yaxt = FALSE, newbies = FALSE) {
@@ -122,8 +120,21 @@ optiPlot2D <- function() {
         
         limits[1] = -9E9; limits[length(limits)] = 9E9
         cols = make_col_vector(cols, limits = limits[1:10])
-        image(x, x, matrix(Pz, ncol = length(x)), breaks = limits, col = cols, 
+
+        Pzm = matrix(Pz, ncol = length(x))
+        image(x, x, Pzm, breaks = limits, col = cols, 
               xaxt = 'n', yaxt = 'n')
+        
+        addHist <- function(side = 1) {
+            h = apply(Pzm, side, sum, na.rm = TRUE)
+            h[h == 0] = NaN
+            h = exp(h - mean(h, na.rm = TRUE))
+            h = h/max(h, na.rm = TRUE)
+            if (side == 1) lines(x, h, lty = 2) else  lines(h, x, lty = 2)
+        }
+        addHist(1)
+        addHist(2)
+
         mtext.units(side = 3, adj = 0.5, title)
         addAxis <- function(side, pt) {
             labs = signif(pt[[1]]$funInverse(seq(0, 1, length.out = 7)),1)
@@ -131,19 +142,22 @@ optiPlot2D <- function() {
             axis(side = side, at = at, label = labs)
         }
         if (xaxt) addAxis(1, param_trans[[1]])
-        if (xlbt) mtext(names(param_trans)[1], line = 2)
-        if (yaxt) { addAxis(2, param_trans[[2]]); mtext(names(param_trans)[2], line = 2)}
+        if (xlbt) mtext.units(names(param_trans)[1], line = 2.2, side = 1)
+        if (yaxt) { 
+            addAxis(2, param_trans[[2]])
+            mtext.units(names(param_trans)[2], line = 1.85, side = 2)
+        }
         if (newbies) points(XY[,2], XY[,3], pch = 17)
         if (!is.null(psZ)) {
             pch = c(19, 4)[PriorTest+1]
-            points(params[1,], params[2,], pch = pch)
+            points(params[1,], params[2,], pch = pch, lwd = 1.2)
             points(params[1,], params[2,], pch = pch, 
                    cex = 0.8, col = cols[cut_results(psZ, limits)-1])
         }
         plot(c(0, 1), c(0, 1), axes = FALSE, xlab = '', ylab = '', type = 'n')
         add_raster_legend2(cols = cols, limits = round(head(limits[-1], -1), 2), 
-                           transpose = FALSE, 
-                           srt = 45, plot_loc = c(0.0, 1.0, 0.8,0.9))
+                           transpose = FALSE, plot_loc = c(0.0, 1.0, 0.25,0.75),
+                           ylabposScling = 3.5)
     } 
  
     imageP(Py, NULL, cols = c('#ffffd9','#edf8b1','#c7e9b4','#7fcdbb','#41b6c4',
@@ -155,85 +169,59 @@ optiPlot2D <- function() {
     imageP(y, ps, cols = c('#fff7f3','#fde0dd','#fcc5c0','#fa9fb5','#f768a1',
                            '#dd3497','#ae017e','#7a0177'), 
            'log(P(~beta~|Y) P(Y))', newbies = TRUE)
-    
-    browser()
-plot(xrange, yrange, type = 'n', xaxt = 'n', xlab = '', ylab = '')
-
-    mtext.units(side = 2, line = 2, 'log(P(~beta~|Y) P(Y))')
-    mtext.units(side = 1, line = 2, '~beta~')
-    
-    labs = signif(param_trans[[1]]$funInverse(seq(0, 1, length.out = 7)),1)
-    at = param_trans[[1]]$fun(labs)
-    axis(side = 1, at = at, label = labs)
-
-    ## adding samples
-    points(params[PriorTest], rescale2By(ps0[PriorTest]), pch = 4, col = 'red')
-    points(params[!PriorTest], rescale2By(ps0[!PriorTest]), pch = 19, col = 'red')
-    points(params[PriorTest], ps[PriorTest], pch = 4, col = 'black')
-    points(params[!PriorTest], ps[!PriorTest], pch = 19, col = 'black')
-
-    ## adding mapped postiriors
-    
-    lines(x, rescale2By(y))
-    lines(x, rescale2By(Py), lty = 2, col = 'blue')
-    lines(x, rescale2By(By), lty = 2, col = 'red')
-
-    mapply(function(x, wd) lines(c(x, x), c(-9E9, 9E9), lty = 3, lwd = wd, col = '#00000099'),
-           X, seq(2, 0.1, length.out = length(X)))
-
+  
     ############
     ## legend ##
     ############  
     par(mar = rep(0, 4))
     plot(c(0, 1), c(0, 1), type = 'n', axes = FALSE, xlab = '', ylab = '')
-
     
-    text.units(c(0.03), 1.05, c('P(Y|~beta~)'), adj = 0.67, xpd = NA)
-    text.units(c(0.1), 1.05, c('P(~beta~|Y)'), adj = 0.33, xpd = NA)
-    points(c(0.03, 0.03), c(0.98, 0.93), pch = c(4, 19), col = 'red')
-    points(c(0.1, 0.1), c(0.98, 0.93), pch = c(4, 19), col = 'black')
-    text.units(c(0.12), c(0.98), c('Seed'), adj = 0)
-    text.units(c(0.12), c(0.93), c('JULES run'), adj = 0)
-
-    addLegLine <- function(x, y = 0.98, txt, ...) {
-        lines(c(x, x + 0.07), c(y, y), ...)
-        text.units(x + 0.1, y, txt, adj = 0)
+    Pnts <- function(x, pch, txt, col = 'white') {
+        Pnt <- function(cex, cl, lwd)
+            points(x, 0.98, pch = pch, cex = cex, col = cl, lwd = lwd)
+        Pnt(1, 'black', 1.2)
+        Pnt(0.5, col, 1)
+        text.units(x + 0.01, 0.98, txt, adj = 0)
     }
-    addLegLine(0.4, txt = 'P(~beta~)', col = 'blue', lty = 2)
-    addLegLine(0.65, txt = 'P(Y|~beta~)', col = 'red', lty = 2)
-    addLegLine(0.4, y = 0.93, 'P(~beta~| Y)')
-
-    lines(c(0.9, 0.9), c(0.99, 0.8), lty = 3, col = '#00000099')
-    text(0.93, 0.9, 'New samples', srt = 90, xpd = NA)
+    Pnts(0.03,  4, 'Seed')
+    Pnts(0.12, 19, 'JULES run')
+    Pnts(0.25, 17, 'New sample', 'black')   
     
     ############################
     ## sample/new sample info ##
     ############################  
-    text.units(x = 0.01, y = 0.83, adj = 0,
-         paste0("Best performance: ", outs[1,which.max(ps)],
+    
+    text.units(x = 0.01, y = 0.9, adj = 0,
+         paste0("Best performance: ", paste0(head(outs[,which.max(ps)],-1), collapse = ', '),
                 "; log(P(~beta~|Y) P(Y)) = ", round(max(ps), 2)))
+    text(x = 0.01, y = 0.82, adj = 0, "New Parameters to test:") 
     
-    
-    text(x = 0.01, y = 0.75, adj = 0, "New Parameters to test:")   
-    X =  round(param_trans[[1]]$funInverse(X), 2)
-    if (length(X) != length(unique(X))) browser()
-    tab1 = cbind(1:length(X), round(X, 2), round(Y, 2))
+    XY[,-1] = mapply(function(F, i) F[[1]]$funInverse(i), param_trans, matrix2list(XY[,2:3]))
+    XY = round(XY, 2)    
+    tab1 = cbind(1:nrow(XY), XY)
     
     AddNews <- function(x, i1, i2) {
         i = i1:i2
-        text(x = x + 0.05, y = 0.67, adj = 0, "Param.")
-        text.units(x = x + 0.15, y = 0.67, adj = 0, "log(P(~beta~|Y) P(Y))")
-    
-        textTfun <- function(i, y, ...)
-            text(i, adj = 0, y = y - seq(0, by = 0.05, length.out = length(i)),...)
-        textTfun(paste0(tab1[i,1], '.'), , x = x, y = 0.6)
-        textTfun(tab1[i,2], x = 0.05 + x, y = 0.6)
-        textTfun(tab1[i,3], x = 0.15 + x, y = 0.6)
-    
+        
+        addCol <- function(vs, dx, pm, y = 0.72, adjc = 0) {        
+            text.units(x = x + dx, y = 0.77, adj = 0, pm)
+            text(vs, x = x + dx, adj = adjc, y = y - seq(0, by = 0.05, length.out = length(vs)))
+        }
+        mapply(addCol , matrix2list(tab1[i, -1]),
+               seq(0.025, length.out = length(param_trans) + 1, by = 0.15),
+               c("log(P(~beta~|Y) P(Y))", names(param_trans)))
+        #text.units(x = x + 0.05 * (1+length(param_trans)), y = 0.67, adj = 0, 
+        #           )
+        addCol(paste0(tab1[i,1], '.'), 0, '', adjc = 1)    
     }
-    i1 = seq(1, nNewParams, by = 10)
+    i1 = seq(1, nNewParams, by = 15)
     i2 = c(tail(i1, -1)-1, nNewParams)
-    xS = seq(0, by = 0.33, length.out = length(i1))
+    xS = seq(0, by = 0.5, length.out = length(i1))
     mapply(AddNews, xS, i1, i2)
+    colnames(XY) = c("log(P(B|Y)P(Y)", gsub('~', '_',  names(param_trans)))
+    gitI = paste0("git URL: ", gitRemoteURL(), "\nversion: ", gitVersionNumber())
+    mtext(side = 4, line = -1, cex = 0.8, gitI, adj = 0)
+    
+    return(XY)
 }
 
